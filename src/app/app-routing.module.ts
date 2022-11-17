b4f4116a07035161854fd71b142fe631f818fcab
victoramattosc <victorcarbelotti0306@gmail.com>
2022-11-17 10:57:07 -0300

    Páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,16 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    pathMatch: 'full'
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+
 
 ];
 @NgModule({
