6c9968c9507a2d48b997410829a4041a3ea34b23
victoramattosc <victorcarbelotti0306@gmail.com>
2022-12-01 08:58:06 -0300

    Cadastro ep3

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormBuilder, FormGroup, Validators} from '@angular/forms' ;
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formCadastro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formCadastro = this.formBuilder.group({
+      nome : ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmaSenha: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+
+    });
+   }
 
   ngOnInit() {
+  }
+
+  salvarCadastro(){
+    console.log('Formulário: ', this.formCadastro.valid);
   }
 
 }
